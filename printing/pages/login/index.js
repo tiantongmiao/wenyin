fb2e870ccd3959345c56be1532fef85a6a63b2e8
laimua <893154844@qq.com>
2021-03-16 00:05:15 +0800

    update 登录页面接口

--- printing/pages/login/index.js
+++ printing/pages/login/index.js
@@ -4,14 +4,23 @@ const app = getApp()
 
 Page({
   data: {
-    
+    loginName: '',
+    password: ''
   },
   // 事件处理函数
   onLoad() {
-    
+  },
+  bindNameInput (e) {
+    this.setData({
+      loginName: e.detail.value
+    })
+  },
+  bindPassWordInput (e) {
+    this.setData({
+      password: e.detail.value
+    })
   },
   toIndex() {
-    console.log(1)
     wx.request({
       url: 'http://192.168.31.147:8089/pas/wx/login', //仅为示例，并非真实的接口地址
       method: 'POST',
@@ -21,17 +30,24 @@ Page({
         'Accept': 'application/json'
       },
       data: {
-        loginName: '',
-        password: ''
+        loginName: this.data.loginName,
+        password: this.data.password
       },
       success (res) {
-        console.log(res.data)
+        if (res.data.code == 0) {
+          // 跳转至首页
+          wx.switchTab({
+            url: '/pages/center/index',
+          })
+        } else {
+          wx.showModal({
+            title: '登录失败',
+            content: res.data.msg,
+            showCancel: false
+          })
+        }
       }
     })
-    // 跳转至首页
-    // wx.switchTab({
-    //   url: '/pages/center/index',
-    // })
   },
   toRegister() {
     wx.navigateTo({
